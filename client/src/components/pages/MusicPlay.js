69dcee97b5e52615db798e4161757b993a9d065e
Nguyễn Tuấn Quốc <quoca2cvp@gmail.com>
2020-01-20 01:21:49 +0700

    add audio control

--- client/src/components/pages/MusicPlay.js
+++ client/src/components/pages/MusicPlay.js
@@ -3,13 +3,18 @@ import { Icon, IconButton } from '@material-ui/core';
 
 import '../../styles/pages/MusicPlay.css';
 
+import audiofile from '../../assets/HappyNewYear-ABBA.mp3';
+
 class MusicPlay extends Component {
     constructor(props) {
         super(props);
         this.state = {
             expandedTab: null,
+            isPlaying: true,
             isMute: false
-        }
+        };
+        this.audio = new Audio(audiofile);
+        // this.audio.play();
     }
     
     async menuExpand(e) {
@@ -32,9 +37,27 @@ class MusicPlay extends Component {
         else currentElement.style.width = '0px';
     }
 
+    audioToggle(e) {
+        if (!this.state.isPlaying) {
+            e.currentTarget.childNodes[0].childNodes[0].innerHTML = "pause";
+            this.audio.play();
+        }
+        else {
+            e.currentTarget.childNodes[0].childNodes[0].innerHTML = "play_arrow";
+            this.audio.pause();
+        }
+        this.setState(() => ({isPlaying: !this.state.isPlaying}));
+    }
+
     changeMute(e) {
-        if (!this.state.isMute) e.currentTarget.childNodes[0].childNodes[0].innerHTML = "music_off";
-        else e.currentTarget.childNodes[0].childNodes[0].innerHTML = "music_note";
+        if (!this.state.isMute) {
+            e.currentTarget.childNodes[0].childNodes[0].innerHTML = "music_off";
+            this.audio.muted = true;
+        }
+        else {
+            e.currentTarget.childNodes[0].childNodes[0].innerHTML = "music_note";
+            this.audio.muted = false;
+        }
         this.setState(() => ({isMute: !this.state.isMute}));
     }
     render() {
@@ -58,7 +81,31 @@ class MusicPlay extends Component {
                     </div>
                 </div>
                 <div className="vertical-pane pane2">
-                    <div className="vertical-pane pane3"></div>
+                    <div className="vertical-pane pane3">
+                        <div className="horizontal-pane pane 1">
+                            <div className="lyrics"></div>
+                        </div>
+                        <div className="horizontal-pane pane2">
+                            <div className="audio-wave"></div>
+                            <div className="audio-control">
+                                <IconButton className="audio-icon-button">
+                                    <Icon fontSize="large">fast_rewind</Icon>
+                                </IconButton>
+                                <IconButton className="audio-icon-button">
+                                    <Icon fontSize="large">skip_previous</Icon>
+                                </IconButton>
+                                <IconButton className="audio-icon-button" onClick={(e) => this.audioToggle(e)}>
+                                    <Icon fontSize="large">pause</Icon>
+                                </IconButton>
+                                <IconButton className="audio-icon-button">
+                                    <Icon fontSize="large">skip_next</Icon>
+                                </IconButton>
+                                <IconButton className="audio-icon-button">
+                                    <Icon fontSize="large">fast_forward</Icon>
+                                </IconButton>
+                            </div>
+                        </div>
+                    </div>
                     <div className="vertical-pane pane4">
                         <div className="songName">
                             <IconButton className="icon-button" onClick={(e) => this.changeMute(e)} style={{color: 'black', width: '40px', height: '40px'}} title="Home">
